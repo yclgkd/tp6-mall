da8bc5fa149ae767a3084fd91554456dfde6996a
Chunlai <chunlai0928@foxmail.com>
2020-03-27 22:57:18 +0800

    '多应用模式'

--- app/model/Demo.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Author: Chunlai<chunlai0928@foxmail.com>
- * Date: 2020/3/27
- * Time: 16:29
- */
--- config/app.php
+++ config/app.php
@@ -31,4 +31,5 @@ return [
     'error_message'    => '页面错误！请稍后再试～',
     // 显示错误信息
     'show_error_msg'   => false,
+    'auto_multi_app' =>  true
 ];
