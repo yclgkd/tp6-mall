5201f43409b592f009172cf9901d7a3eca0e3d85
Chunlai <chunlai0928@foxmail.com>
2020-04-03 17:19:36 +0800

    '添加mysql状态码'

--- config/status.php
+++ config/status.php
@@ -11,5 +11,11 @@ return [
     "not_login" => -1,
     "user_is_register" => -2,
     "action_not_found" => -3,
-    "controller_not_found" => -4
+    "controller_not_found" => -4,
+
+    "mysql" => [
+        "table_normal" => 1,
+        "table_pending" => 0,
+        "table_delete" => 99
+    ],
 ];
