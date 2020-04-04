cb13bda7d9015a5a566e402e2fc1af56721597a2
Chunlai <chunlai0928@foxmail.com>
2020-04-04 17:37:53 +0800

    '利用前置中间件处理登录流'

--- app/admin/middleware/Auth.php
+++ app/admin/middleware/Auth.php
@@ -12,11 +12,15 @@ class Auth
     public function handle($request, \Closure $next)
     {
         // 前置中间件
-        $response = $next($request);
-        // 当退出后跳转登录页面
-        if (empty(session(config("admin.session_admin"))) && $request->controller() != "Login") {
+        if (empty(session(config("admin.session_admin"))) && !preg_match("/login/", $request->pathinfo())) {
             return redirect((string)url("login/index"));
         }
+
+        $response = $next($request);
+        // 当退出后跳转登录页面
+        //if (empty(session(config("admin.session_admin"))) && $request->controller() != "Login") {
+            //return redirect((string)url("login/index"));
+        //}
         return $response;
         // 后置中间件
     }
